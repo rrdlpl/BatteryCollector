315bbd713181cedc843e7aede50a6fd0f7a279d7
Raven <rrdlpl@gmail.com>
2017-01-01 23:18:46 -0500

    Initial commit

---
+++ Source/BatteryCollector/BatteryCollectorGameMode.h
@@ -0,0 +1,16 @@
+// Copyright 1998-2016 Epic Games, Inc. All Rights Reserved.
+#pragma once
+#include "GameFramework/GameModeBase.h"
+#include "BatteryCollectorGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ABatteryCollectorGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ABatteryCollectorGameMode();
+};
+
+
+
